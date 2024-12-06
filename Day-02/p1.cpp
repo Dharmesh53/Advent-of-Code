225b66c5f74b4963d4a3ed9227276e4afac55f7f
Dharmesh53 <dhiru7321r@gmail.com>
2024-12-06 16:31:37 +0530

    chore: Day 02 P1

--- Day-02/p1.cpp
+++ Day-02/p1.cpp
@@ -5,22 +5,47 @@
 #include <vector>
 using namespace std;
 
+void print(vector<int> &vt) {
+  for (auto num : vt) {
+    cout << num << " ";
+  }
+  cout << "\n";
+}
+
+auto isIncreasing = [](int a, int b) -> bool { return a < b; };
+
+auto isDecreasing = [](int a, int b) -> bool { return a > b; };
+
+auto isEqual = [](int a, int b) -> bool { return a == b; };
+
+auto inRange = [](int a, int b) -> bool {
+  int diff = abs(a - b);
+  return diff >= 1 && diff <= 3;
+};
+
 bool isSafe(vector<int> &nums) {
   if (nums.size() == 1)
     return true;
 
-  bool isIncreasing = nums[0] < nums[1];
+  if (isEqual(nums[0], nums[1]) || !inRange(nums[0], nums[1])) {
+    return false;
+  }
+
+  bool isArrayIncreasing = nums[0] < nums[1];
 
   for (int i = 2; i < nums.size(); i++) {
-    if ((nums[i] < nums[i - 1] && isIncreasing) ||
-        (nums[i] > nums[i - 1] && !isIncreasing)) {
+    if (!inRange(nums[i], nums[i - 1])) {
       return false;
     }
 
-    if ((abs(nums[i] - nums[i - 1]) < 1) || (abs(nums[i] - nums[i - 1]) > 3)) {
+    if ((nums[i] == nums[i - 1]) ||
+        (isDecreasing(nums[i - 1], nums[i]) && isArrayIncreasing) ||
+        (isIncreasing(nums[i - 1], nums[i]) && !isArrayIncreasing)) {
       return false;
     }
   }
+
+  print(nums);
 
   return true;
 }
